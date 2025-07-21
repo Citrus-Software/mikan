e2a3871d520109182de13a546cafba6c4631ee97
Thomas Guittonneau <thomas.guittonneau@teamto.com>
2025-07-21 15:40:46 +0200

    refactor: cleanup YAML Dumper/Loader to avoid global overrides

--- src/mikan/core/utils/yamlutils.py
+++ src/mikan/core/utils/yamlutils.py
@@ -11,34 +11,40 @@ is_python_3 = (sys.version_info[0] == 3)
 __all__ = ['YamlDumper', 'YamlLoader', 'ordered_dump', 'ordered_load']
 
 try:
-    class YamlLoader(yaml.CLoader):
-        pass
-except:
-    class YamlLoader(yaml.Loader):
-        pass
+    BaseLoader = yaml.CSafeLoader
+except AttributeError:
+    BaseLoader = yaml.SafeLoader
 
 try:
-    class YamlDumper(yaml.CDumper):
-        pass
-except:
-    class YamlDumper(yaml.Dumper):
-        pass
+    BaseDumper = yaml.CSafeDumper
+except AttributeError:
+    BaseDumper = yaml.SafeDumper
 
 
-def _construct_mapping(loader, node):
+class YamlLoader(BaseLoader):
+    pass
+
+
+class YamlDumper(BaseDumper):
+    pass
+
+
+# safe unicode
+if not is_python_3:
+    yaml.add_representer(unicode, SafeRepresenter.represent_unicode, Dumper=YamlDumper)
+
+
+# safe ordered dict
+def _construct_ordered_mapping(loader, node):
     loader.flatten_mapping(node)
     return ordered_dict(loader.construct_pairs(node))
 
 
-YamlLoader.add_constructor(
-    yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG,
-    _construct_mapping
-)
-
 if not is_python_3:
-    YamlDumper.add_representer(unicode, SafeRepresenter.represent_unicode)
-else:
-    YamlDumper = yaml.Dumper
+    YamlLoader.add_constructor(
+        yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG,
+        _construct_ordered_mapping,
+    )
 
 
 def _dict_representer(dumper, data):
@@ -48,13 +54,32 @@ def _dict_representer(dumper, data):
     )
 
 
-YamlDumper.add_representer(dict, _dict_representer)
-YamlDumper.add_representer(OrderedDict, _dict_representer)
+if not is_python_3:
+    yaml.add_representer(dict, _dict_representer, Dumper=YamlDumper)
+    yaml.add_representer(OrderedDict, _dict_representer, Dumper=YamlDumper)
 
 
+# clean number list
+def _number_sequence_representer(dumper, data):
+    if all(isinstance(i, (float, int)) for i in data):
+        return dumper.represent_sequence(
+            yaml.resolver.BaseResolver.DEFAULT_SEQUENCE_TAG,
+            data,
+            flow_style=True,
+        )
+    return dumper.represent_sequence(
+        yaml.resolver.BaseResolver.DEFAULT_SEQUENCE_TAG,
+        data,
+    )
+
+
+yaml.add_representer(list, _number_sequence_representer, Dumper=YamlDumper)
+
+
+# custom loader/dumper
 def ordered_load(stream, Loader=YamlLoader):
-    return yaml.load(stream, Loader)
+    return yaml.load(stream, Loader=Loader)
 
 
 def ordered_dump(data, stream=None, Dumper=YamlDumper, **kwds):
-    return yaml.dump(data, stream, Dumper, **kwds)
+    return yaml.dump(data, stream, Dumper=Dumper, **kwds)
